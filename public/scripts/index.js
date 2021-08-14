85d74e86d99f1feef87cbc89b8711948bacb7507
Mathias Mathisen <mathimat@raymannair.lan>
2021-08-15 00:18:13 +0200

    styling

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,37 @@
-const Calculator = {
-    data() {
+const App = {
+    data(){
         return {
-            result: '100kr/pax'
+            fields: [
+                {field: 'kilometers', label: 'Kilometers Driven', tooltip: 'google maps'},
+                {field: 'passengers', label: 'Number of Passengers', tooltip: 'driver excluded'},
+                {field: 'energyprkm', label: 'Wh/km', tooltip: ''},
+                {field: 'energyprice', label: 'kr/Wh', tooltip: ''},
+                {field: 'marginal cost/km', label: 'kr/km', tooltip: 'Tire weare, maintanance etc.'},
+                {field: 'parking/tolls', label: 'kr', tooltip: ''}
+            ]
         }
     }
 }
 
-Vue.createApp(Calculator).mount('#content');
+
+const app = Vue.createApp(App);
+
+app.component('input-field', {
+    data() {
+        return {
+            value: 0
+        }
+    },
+    props: ['field','label','tooltip'],
+    template: `
+    <div class="input-data">
+        <label>{{label}}
+            <input v-model.number="value" type="number">
+        </label>
+        <div class="input-tooltip">{{tooltip}}</div>
+    </div>
+    `
+});
+
+
+app.mount('#content');
